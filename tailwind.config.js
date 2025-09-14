66134e0193d9331f0340bc5af4bcdbd459105098
Matthew An <hailan-an@idexx.com>
2025-09-14 14:51:18 +1200

    Update color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,23 @@ module.exports = {
     extend: {
       colors: {
         gray: colors.slate,
-        primary: colors.rose,
-        secondary: colors.indigo,
+        primary: {
+          50: '#f0f7ff',
+          100: '#e0efff',
+          200: '#bae0ff',
+          300: '#7cc8ff',
+          400: '#36adff',
+          500: '#0b94ff',
+          600: '#1C459D',
+          700: '#1a3d8a',
+          800: '#1c3570',
+          900: '#1d2f5d',
+          950: '#161e3e',
+        },
+        secondary: colors.cyan,
         tertiary: colors.slate,
         danger: colors.red,
+        success: colors.emerald,
       },
       fontFamily: {
         sans: ['Work Sans','Inter var', ...defaultTheme.fontFamily.sans]
